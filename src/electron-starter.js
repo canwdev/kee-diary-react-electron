a73b4bce180836f11f776a468e160221387ab76a
canwdev <c2097@live.com>
2019-11-18 15:33:18 +0800

    限制 APP 单实例

--- src/electron-starter.js
+++ src/electron-starter.js
@@ -1,26 +1,13 @@
+process.env['ELECTRON_DISABLE_SECURITY_WARNINGS'] = 'true';
 // Modules to control application life and create native browser window
 const {app, BrowserWindow} = require('electron')
 const path = require('path')
 const url = require('url')
-
-function judgementElectronIsDev() {
-  const electron = require('electron');
-
-  const app = electron.app || electron.remote.app;
-
-  const isEnvSet = 'ELECTRON_IS_DEV' in process.env;
-  const getFromEnv = parseInt(process.env.ELECTRON_IS_DEV, 10) === 1;
-
-  return isEnvSet ? getFromEnv : !app.isPackaged;
-}
-
 const isDev = judgementElectronIsDev()
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
-
-process.env['ELECTRON_DISABLE_SECURITY_WARNINGS'] = 'true';
 
 function createWindow() {
 
@@ -79,24 +66,49 @@ function createWindow() {
 
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+// 限制 APP 单实例
+const gotTheLock = app.requestSingleInstanceLock()
+if (!gotTheLock) {
+  app.quit()
+} else {
+  app.on('second-instance', (event, commandLine, workingDirectory) => {
+    // Someone tried to run a second instance, we should focus our window.
+    if (mainWindow) {
+      if (mainWindow.isMinimized()) mainWindow.restore()
+      mainWindow.focus()
+    }
+  })
 
-// Quit when all windows are closed.
-app.on('window-all-closed', function () {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') app.quit()
-})
+  // This method will be called when Electron has finished
+  // initialization and is ready to create browser windows.
+  // Some APIs can only be used after this event occurs.
+  app.on('ready', createWindow)
 
-app.on('activate', function () {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) createWindow()
-})
+  // Quit when all windows are closed.
+  app.on('window-all-closed', function () {
+    // On macOS it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform !== 'darwin') app.quit()
+  })
+
+  app.on('activate', function () {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (mainWindow === null) createWindow()
+  })
+
+}
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
 
+function judgementElectronIsDev() {
+  const electron = require('electron');
+
+  const app = electron.app || electron.remote.app;
+
+  const isEnvSet = 'ELECTRON_IS_DEV' in process.env;
+  const getFromEnv = parseInt(process.env.ELECTRON_IS_DEV, 10) === 1;
+
+  return isEnvSet ? getFromEnv : !app.isPackaged;
+}
